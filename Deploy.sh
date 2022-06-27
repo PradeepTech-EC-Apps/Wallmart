72549bd5cd0576e4ba4c5f10776a5476c27e965e
PradeepTehnologies <Devops@gmail.com>
2022-06-27 08:28:11 +0530

    updated one

--- Deploy.sh
+++ Deploy.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 
 
@@ -14,5 +15,10 @@
 
 This code is updated from stage branch 
 Im from Master 
+=======
+Im from Master 
+
+
+>>>>>>> 31a3e09... Master updated
 Hello This filw contain deployment ship
  thiss is update for change
